cc80bfc3895af7e883115744b020f4c0493b245f
georgiouc <chris.cg895@gmail.com>
2024-10-30 10:44:14 +0200

    power_menu version 1.1

--- .config/i3/power_menu.sh
+++ .config/i3/power_menu.sh
@@ -4,7 +4,9 @@
 MENU_FILE="/tmp/i3-power-menu"
 
 # Generate menu options
-echo "Sleep" > $MENU_FILE
+echo "Lock" > $MENU_FILE
+echo "Sleep" >> $MENU_FILE
+echo "Hibernate" >> $MENU_FILE
 echo "Restart" >> $MENU_FILE
 echo "Shutdown" >> $MENU_FILE
 
@@ -13,8 +15,14 @@ CHOICE=$(cat $MENU_FILE | dmenu -p "Select an option:")
 
 # Act on the choice
 case $CHOICE in
+    Lock)
+        i3lock -c 000000
+        ;;
     Sleep)
         systemctl suspend
+        ;;
+    Hibernate)
+        systemctl hibernate
         ;;
     Restart)
         systemctl reboot
