7518ee4477311cd2122530c344efdbf501295b50
georgiouc <chris.cg895@gmail.com>
2024-11-19 12:57:08 +0200

    added xarchiver to unzip .zip files

--- dependencies.sh
+++ dependencies.sh
@@ -4,7 +4,7 @@
 sudo apt update
 
 #Install dependencies 
-sudo apt install -y i3 i3status i3lock parcellite feh dex picom nitrogen x11-xserver-utils network-manager-gnome brightnessctl pulseaudio pavucontrol thunar brave-browser flameshot git
+sudo apt install -y i3 i3status i3lock parcellite feh dex picom nitrogen x11-xserver-utils network-manager-gnome brightnessctl pulseaudio pavucontrol thunar xarchiver brave-browser flameshot git
 
 nitrogen --set-zoom-fill ~/Pictures/Wallpapers/default_wallpaper.jpg
 
